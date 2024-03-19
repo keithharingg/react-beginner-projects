ca09d9a10389483ba12d17839450839a0a1030a8
Taras Nesterenko <tnestere@gmail.com>
2024-03-19 15:35:46 +0100

    Added setStep

--- src/App.js
+++ src/App.js
@@ -1,58 +1,71 @@
+import React, { useState } from 'react';
 import './index.scss';
 
 const questions = [
   {
-    title: 'React - это ... ?',
-    variants: ['библиотека', 'фреймворк', 'приложение'],
+    title: 'React is a ... ?',
+    variants: ['library', 'framework', 'application'],
     correct: 0,
   },
   {
-    title: 'Компонент - это ... ',
-    variants: ['приложение', 'часть приложения или страницы', 'то, что я не знаю что такое'],
+    title: 'Component is ... ',
+    variants: ['application', 'part of an app or page', "is that I don't know what it is"],
     correct: 1,
   },
   {
-    title: 'Что такое JSX?',
+    title: 'What is JSX?',
     variants: [
-      'Это простой HTML',
-      'Это функция',
-      'Это тот же HTML, но с возможностью выполнять JS-код',
+      'It is a simple HTML',
+      'Function',
+      "It's the same HTML, but with the ability to execute JS code",
     ],
     correct: 2,
   },
 ];
 
-function Result() {
+function Result({ setStep }) {
   return (
     <div className="result">
-      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-      <h2>Вы отгадали 3 ответа из 10</h2>
-      <button>Попробовать снова</button>
+      <img alt="congrats" src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>You guessed 3 out of 10 answers</h2>
+      <button onClick={() => setStep(0)}>Try again</button>
     </div>
   );
 }
 
-function Game() {
+function Game({ question, onClickVariant, step }) {
+  const percentage = Math.round((step / questions.length) * 100);
   return (
     <>
       <div className="progress">
-        <div style={{ width: '50%' }} className="progress__inner"></div>
+        <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
       </div>
-      <h1>Что такое useState?</h1>
+      <h1>{question.title}</h1>
       <ul>
-        <li>Это функция для хранения данных компонента</li>
-        <li>Это глобальный стейт</li>
-        <li>Это когда на ты никому не нужен</li>
+        {question.variants.map((text, index) => (
+          <li onClick={() => onClickVariant(index)} key={index}>
+            {text}
+          </li>
+        ))}
       </ul>
     </>
   );
 }
 
 function App() {
+  const [step, setStep] = useState(0);
+  const question = questions[step];
+  const onClickVariant = (index) => {
+    console.log(step, index);
+    setStep(step + 1);
+  };
   return (
     <div className="App">
-      <Game />
-      {/* <Result /> */}
+      {step !== questions.length ? (
+        <Game step={step} onClickVariant={onClickVariant} question={question} />
+      ) : (
+        <Result setStep={setStep} />
+      )}
     </div>
   );
 }
